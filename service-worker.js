c62f827bc43af09a7decbfd1f9233df33e0e2850
rappstorecom <rajibulislam6fil@gmail.com>
2025-10-21 09:09:04 +0530

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "neo-converter-v1";
+const urlsToCache = [
+  "neo_converter/",
+  "neo_converter/index.html",
+  "neo_converter/style.css",
+  "neo_converter/script.js",
+  "neo_converter/manifest.json",
+  "neo_converter/assets/icon.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Resources
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
